aa98d0a9fe594b2d9b50cb77d249969017b27165
rajeevchoudhary067@gmail.com <91446278+rajeevchoudhary035@users.noreply.github.com>
2024-08-20 12:40:40 +0530

    clientur

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // Proxy API requests
-      '/api1': {
-        target: 'https://serverapi-2.vercel.app', // The backend server URL
-        changeOrigin: true,
-        secure: true, // If the backend server is using HTTPS, set this to true
-        rewrite: (path) => path.replace(/^\/api/, '') // Optional: rewrite the URL path
-      }
-    }
-  }
-});
+})
